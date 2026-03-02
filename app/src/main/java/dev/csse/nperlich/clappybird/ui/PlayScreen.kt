142e13599938bdf5d20101c32dd3282b5ac6512d
NickPerlich <64299263+NickPerlich@users.noreply.github.com>
2026-03-02 15:16:41 -0800

    removed screen darkening on tap

--- app/src/main/java/dev/csse/nperlich/clappybird/ui/PlayScreen.kt
+++ app/src/main/java/dev/csse/nperlich/clappybird/ui/PlayScreen.kt
@@ -2,6 +2,7 @@ package dev.csse.nperlich.clappybird.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -14,6 +15,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.csse.nperlich.clappybird.GameViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @Preview(
     showBackground = true,
@@ -41,7 +44,12 @@ fun PlayScreen(
     Box(
         modifier = modifier
             .fillMaxSize()
-            .clickable { viewModel.flap() }
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                viewModel.flap()
+            }
     ) {
         // Bird - yellow circle
         Box(
